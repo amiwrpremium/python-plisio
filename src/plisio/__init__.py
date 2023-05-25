688ef189f47f60adf7024dfde451249236d53bec
amiwrpremium <83715695+amiwrpremium@users.noreply.github.com>
2023-05-25 03:40:30 +0330

    updated
    added version
    pre commit fixed
    pypeoject updated

--- src/plisio/__init__.py
+++ src/plisio/__init__.py
@@ -3,5 +3,6 @@ Plisio Python SDK.
 """
 
 from .clients import Client, AsyncClient
+from ._meta import __version__
 
-__version__ = "0.1.0"
+# __version__ = "0.1.0"
---
+++ src/plisio/_meta.py
@@ -0,0 +1,12 @@
+"""
+Metadata for plisio.
+"""
+
+from importlib.metadata import version as _version
+
+__version__ = _version("python-plisio")
+__author__ = "AMiWR"
+__license__ = "MIT"
+__repo__ = "https://github.com/amiwrpremium/python-plisio"
+__description__ = "Plisio Python SDK."
+__email__ = "amiwrpremium@gmail.com"
